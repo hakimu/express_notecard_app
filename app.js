74f5f627a2fb5d308697e3bc1dce25684e0d03f9
Hakimu Jackson <hakimu@newrelic.com>
2014-10-16 22:32:20 -0700

    Revised to add to db

--- app.js
+++ app.js
@@ -3,8 +3,27 @@ var express = require('express');
 
 var app = express();
 
-app.use(function(req,res,next){
-	res.send('Yeah, this is the notecard APP');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/notecard');
+
+var Notecard = require('./models/notecard');
+
+// app.use(function(req,res,next){
+// 	res.send('Yeah, this is the notecard APP');
+// });
+
+app.get('/', function(req,res){
+	res.send("The new route")
 });
+
+app.get('/notecard', function(req,res){
+	Notecard.find(function(err, notecard){
+		if (err)
+			res.send(err);
+		res.json(notecard)
+	});
+});
+
+// app.use()
 app.listen(3000);
 console.log('Let the games begin...on port 3000');
--- models/notecard.js
+++ models/notecard.js
@@ -11,3 +11,36 @@ var noteCardSchema = new Schema({
 
 module.exports = mongoose.model('Notecard', noteCardSchema);
 
+var Note = mongoose.model('Notecard');
+
+var note = new Note();
+
+note.term = 'Node.js';
+note.definition = "Platform built on the Google V8 runtime";
+note.subject = "Languages";
+note.dateCreated = Date.now();
+note.dateUpdated = Date.now();
+note.save(function(err, note_Saved){
+	if(err){
+		throw err;
+		console.log(err);
+	} else {
+		console.log('saved!');
+	}
+});
+
+var second_note = new Note();
+
+second_note.term = 'Ruby';
+second_note.definition = "An object oriented programming language";
+second_note.subject = "Languages";
+second_note.dateCreated = Date.now();
+second_note.dateUpdated = Date.now();
+second_note.save(function(err, note_Saved){
+	if(err){
+		throw err;
+		console.log(err);
+	} else {
+		console.log('saved!');
+	}
+});
